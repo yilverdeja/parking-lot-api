5fe08215bd2078e12fdc4a1c8eb933186bc16fcb
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-08-31 01:43:59 +0800

    fix: naming issues

--- src/models/parkinglot.ts
+++ src/models/parkinglot.ts
@@ -1,57 +1,57 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export interface IParkingLot {
-	_id: string;
-	name: string;
-	location: string;
-	hourlyCost: number;
-	lots: boolean[];
-	capacity: number;
-	lotsOccupied: number;
-	numDrivers: number;
-	createdAt: Date;
-	updatedAt: Date;
+  _id: string;
+  name: string;
+  location: string;
+  hourlyCost: number;
+  lots: boolean[];
+  capacity: number;
+  lotsOccupied: number;
+  numDrivers: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const parkingLotSchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		location: {
-			type: String,
-			required: true,
-		},
-		hourlyCost: {
-			type: Number,
-			required: true,
-		},
-		capacity: {
-			type: Number,
-			required: true,
-		},
-		lots: [
-			{
-				type: Boolean,
-				required: true,
-				default: false,
-			},
-		],
-		numDrivers: {
-			type: Number,
-			default: 0,
-		},
-		lotsOccupied: {
-			type: Number,
-			default: 0,
-		},
-	},
-	{ timestamps: true }
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    hourlyCost: {
+      type: Number,
+      required: true,
+    },
+    capacity: {
+      type: Number,
+      required: true,
+    },
+    lots: [
+      {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+    ],
+    numDrivers: {
+      type: Number,
+      default: 0,
+    },
+    lotsOccupied: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
 );
 
 const ParkingLot = mongoose.model<IParkingLot & mongoose.Document>(
-	'ParkingLot',
-	parkingLotSchema
+  "ParkingLot",
+  parkingLotSchema
 );
 export default ParkingLot;
--- src/services/parkingLotService.ts
+++ src/services/parkingLotService.ts
@@ -1,105 +1,105 @@
-import { AppError } from '../utils/errors';
-import ParkingLot from '../models/parkingLot';
-import { LotInformation, ParkingLotGeneralOccupancy } from '../utils/types';
+import { AppError } from "../utils/errors";
+import ParkingLot from "../models/parkingLot";
+import { LotInformation, ParkingLotGeneralOccupancy } from "../utils/types";
 
 class ParkingLotService {
-	async getAllParkingLots() {
-		return await ParkingLot.find().sort('name');
-	}
+  async getAllParkingLots() {
+    return await ParkingLot.find().sort("name");
+  }
 
-	async getParkingLotById(id: string) {
-		const parkingLot = await ParkingLot.findById(id);
-		if (!parkingLot) throw new AppError('Parking lot not found', 404);
-		return parkingLot;
-	}
+  async getParkingLotById(id: string) {
+    const parkingLot = await ParkingLot.findById(id);
+    if (!parkingLot) throw new AppError("Parking lot not found", 404);
+    return parkingLot;
+  }
 
-	async createParkingLot(
-		name: string,
-		location: string,
-		hourlyCost: number,
-		capacity: number
-	) {
-		const lots: boolean[] = [];
-		for (let i = 1; i <= capacity; i++) {
-			lots.push(false);
-		}
-		return await ParkingLot.create({
-			name,
-			location,
-			hourlyCost,
-			capacity,
-			lots,
-		});
-	}
+  async createParkingLot(
+    name: string,
+    location: string,
+    hourlyCost: number,
+    capacity: number
+  ) {
+    const lots: boolean[] = [];
+    for (let i = 1; i <= capacity; i++) {
+      lots.push(false);
+    }
+    return await ParkingLot.create({
+      name,
+      location,
+      hourlyCost,
+      capacity,
+      lots,
+    });
+  }
 
-	async updateParkingLotHourlyCost(id: string, hourlyCost: number) {
-		const parkingLot = await ParkingLot.findByIdAndUpdate(
-			id,
-			{ hourlyCost },
-			{ new: true }
-		);
-		if (!parkingLot) throw new AppError('Parking lot not found', 404);
-		return parkingLot;
-	}
+  async updateParkingLotHourlyCost(id: string, hourlyCost: number) {
+    const parkingLot = await ParkingLot.findByIdAndUpdate(
+      id,
+      { hourlyCost },
+      { new: true }
+    );
+    if (!parkingLot) throw new AppError("Parking lot not found", 404);
+    return parkingLot;
+  }
 
-	async deleteParkingLot(id: string) {
-		const parkingLot = await ParkingLot.findByIdAndDelete(id);
-		if (!parkingLot) throw new AppError('Parking lot not found', 404);
-		return parkingLot;
-	}
+  async deleteParkingLot(id: string) {
+    const parkingLot = await ParkingLot.findByIdAndDelete(id);
+    if (!parkingLot) throw new AppError("Parking lot not found", 404);
+    return parkingLot;
+  }
 
-	async occupyLot(id: string, lotPosition: number): Promise<LotInformation> {
-		const parkingLot = await this.getParkingLotById(id);
+  async occupyLot(id: string, lotPosition: number): Promise<LotInformation> {
+    const parkingLot = await this.getParkingLotById(id);
 
-		if (lotPosition >= parkingLot.lots.length || lotPosition < 0) {
-			throw new AppError('This lot number does not exist', 404);
-		}
+    if (lotPosition >= parkingLot.lots.length || lotPosition < 0) {
+      throw new AppError("This lot number does not exist", 404);
+    }
 
-		if (parkingLot.lots[lotPosition]) {
-			throw new AppError('Lot is already occupied', 400);
-		}
+    if (parkingLot.lots[lotPosition]) {
+      throw new AppError("Lot is already occupied", 400);
+    }
 
-		parkingLot.lots[lotPosition] = true;
-		parkingLot.lotsOccupied++;
-		await parkingLot.save();
+    parkingLot.lots[lotPosition] = true;
+    parkingLot.lotsOccupied++;
+    await parkingLot.save();
 
-		return {
-			lotPosition: lotPosition,
-			occupied: parkingLot.lots[lotPosition],
-			lotsOccupied: parkingLot.lotsOccupied,
-		};
-	}
+    return {
+      lotPosition: lotPosition,
+      occupied: parkingLot.lots[lotPosition],
+      lotsOccupied: parkingLot.lotsOccupied,
+    };
+  }
 
-	async releaseLot(id: string, lotPosition: number): Promise<LotInformation> {
-		const parkingLot = await this.getParkingLotById(id);
+  async releaseLot(id: string, lotPosition: number): Promise<LotInformation> {
+    const parkingLot = await this.getParkingLotById(id);
 
-		if (lotPosition >= parkingLot.lots.length || lotPosition < 0) {
-			throw new AppError('This lot number does not exist', 404);
-		}
+    if (lotPosition >= parkingLot.lots.length || lotPosition < 0) {
+      throw new AppError("This lot number does not exist", 404);
+    }
 
-		if (!parkingLot.lots[lotPosition]) {
-			throw new AppError('Lot is already unoccupied', 400);
-		}
+    if (!parkingLot.lots[lotPosition]) {
+      throw new AppError("Lot is already unoccupied", 400);
+    }
 
-		parkingLot.lots[lotPosition] = false;
-		parkingLot.lotsOccupied--;
-		await parkingLot.save();
+    parkingLot.lots[lotPosition] = false;
+    parkingLot.lotsOccupied--;
+    await parkingLot.save();
 
-		return {
-			lotPosition: lotPosition,
-			occupied: parkingLot.lots[lotPosition],
-			lotsOccupied: parkingLot.lotsOccupied,
-		};
-	}
+    return {
+      lotPosition: lotPosition,
+      occupied: parkingLot.lots[lotPosition],
+      lotsOccupied: parkingLot.lotsOccupied,
+    };
+  }
 
-	async getGeneralOccupancy(id: string): Promise<ParkingLotGeneralOccupancy> {
-		const parkingLot = await this.getParkingLotById(id);
-		return {
-			capacity: parkingLot.capacity,
-			numDrivers: parkingLot.numDrivers,
-			lotsOccupied: parkingLot.lotsOccupied,
-		};
-	}
+  async getGeneralOccupancy(id: string): Promise<ParkingLotGeneralOccupancy> {
+    const parkingLot = await this.getParkingLotById(id);
+    return {
+      capacity: parkingLot.capacity,
+      numDrivers: parkingLot.numDrivers,
+      lotsOccupied: parkingLot.lotsOccupied,
+    };
+  }
 }
 
 export default new ParkingLotService();
